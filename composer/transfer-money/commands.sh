37d7bb059825a2fffe14f94b23ef159e6b77ccb4
Palwinder <pdeol@deakin.edu.au>
2019-08-27 09:58:37 +1000

    query file added

---
+++ composer/transfer-money/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ..
+
+./stopFabric.sh
+
+./startFabric.sh
+
+cd transfer-money
+
+composer archive create -t dir -n .
+
+composer network install --card PeerAdmin@hlfv1 --archiveFile transfer-money@0.0.11.bna
+
+composer network start --networkName transfer-money --networkVersion 0.0.11 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+
+composer-rest-server -c admin@transfer-money -n never -u true -d y
