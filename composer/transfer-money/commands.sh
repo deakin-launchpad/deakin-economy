f6a747a18a7cd84e9078e6ee381a28d114de4f08
Ankit <sehgalan@deakin.edu.au>
2019-08-27 04:01:12 +0000

    included fabric scripts

--- composer/transfer-money/commands.sh
+++ composer/transfer-money/commands.sh
@@ -9,8 +9,10 @@ cd transfer-money
 
 composer archive create -t dir -n .
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile transfer-money@0.0.11.bna
+composer network install --card PeerAdmin@hlfv1 --archiveFile transfer-money@0.0.1.bna
 
-composer network start --networkName transfer-money --networkVersion 0.0.11 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
-
+composer network start --networkName transfer-money --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+cd dist
+composer card import --file networkadmin.card
+cd ..
 composer-rest-server -c admin@transfer-money -n never -u true -d y
